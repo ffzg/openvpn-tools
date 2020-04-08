37c3ec739b3f3b3b252e5c2c8b4cdc8be396b7d1
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-04-08 10:18:05 +0200

    display users unsing openvpn management socket

---
+++ openvpn-top.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use autodie;
+
+use IO::Socket::INET;
+use Data::Dump qw(dump); # sudo apt install libdata-dump-perl
+
+my $debug = $ENV{DEBUG} || 0;
+
+my $sock = IO::Socket::INET->new(PeerAddr => 'localhost',
+	PeerPort => 2000, # openvpn --management 127.0.0.1 2000
+	Proto    => 'tcp') || die "can't connect: $!";
+
+sub in {
+	my $expect = shift;
+	my $in = <$sock>;
+	chomp($in);
+	warn "<< [$in]\n" if $debug;
+	return $in;
+}
+
+my $in = in;
+die "expect >INFO" unless $in =~ m/^>INFO/;
+
+my $status;
+
+print $sock "status\n";
+while( <$sock> ) {
+	s/[\r\n]+$//;
+	warn "<< [$_]\n" if $debug;
+	last if m/^END/;
+
+	my @v = split(/,/,$_);
+	next if $v[0] =~ m/\s+/; # header labels have spaces
+
+	if ( $#v == 4 ) {
+		$status->{ $v[0] } = {
+			client => $v[1],
+			recv => $v[2],
+			sent => $v[3],
+			since => $v[4]
+		}
+	} elsif ( $#v == 3 ) {
+		$status->{ $v[1] }->{ip} = $v[0];
+	}
+
+}
+
+print $sock "quit\n";
+
+close($sock);
+
+warn "# status = ",dump($status) if $debug;
+
+my @cols = qw( client ip recv sent since );
+
+foreach my $login ( sort keys %$status ) {
+	my $u = $status->{$login};
+	printf("%-10s %-20s %-10s %10s %10s %s\n", $login, map { $u->{$_} } @cols);
+}
